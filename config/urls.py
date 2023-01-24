7e60b516ebaf97002dd44f97ade7abd80d7f6488
EC2 Default User <ec2-user@ip-172-31-81-95.ec2.internal>
2023-01-24 00:34:47 +0000

    リダイレクトを設定

--- config/urls.py
+++ config/urls.py
@@ -4,10 +4,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("items/", include("items.urls")),
+    path("", views.index),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
