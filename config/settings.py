5a77c1062dc3c2f1717dddcc66eb30e714d9a9a2
EC2 Default User <ec2-user@ip-172-31-2-60.ec2.internal>
2023-11-20 11:23:14 +0000

    takuya

--- config/settings.py
+++ config/settings.py
@@ -26,8 +26,9 @@ SECRET_KEY = 'f%axi2zlyspuh8kh0kqmv$yni(n%@2j*v599h6l=mlm%dkwl$u'
 DEBUG = True
 
 ALLOWED_HOSTS = ["8d6505a2d4544c27be984d7d7f97653a.vfs.cloud9.us-east-1.amazonaws.com",
-                 "4fffdffa8f0244efb6c00b6acae26f60.vfs.cloud9.us-east-1.amazonaws.com"
-                 "231c042d19b445b7800e17e44b1cddae.vfs.cloud9.us-east-1.amazonaws.com"]
+                 "4fffdffa8f0244efb6c00b6acae26f60.vfs.cloud9.us-east-1.amazonaws.com",
+                 "231c042d19b445b7800e17e44b1cddae.vfs.cloud9.us-east-1.amazonaws.com",
+                 "c125b0ec6ea245dda5e504819d79257b.vfs.cloud9.us-east-1.amazonaws.com"]
 
 
 # Application definition
