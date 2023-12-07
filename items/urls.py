f08d956bbab78a0272ccb83f9df3b64635e70c67
[ユーザー名] <[sarah]>
2023-12-07 14:35:09 +0000

    微調整

--- items/urls.py
+++ items/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "items"
  
 urlpatterns = [
-    path("", views.Index.as_view(), name="index"), # トップページ
+    path("", views.Item_list.as_view(), name="index"), # 商品一覧画面
     path("admin/", views.Admin.as_view(), name="admin"), # 管理画面
     path("create/", views.Create.as_view(), name="create"), # 新規作成
     path("create_item_type/", views.Create_item_type.as_view(), name="create_item_type"),
@@ -20,7 +20,6 @@ urlpatterns = [
     path("delete_image/<int:pk>/", views.Delete_image.as_view(), name="delete_image"),
     path("update/<int:pk>/", views.Update.as_view(), name="update"), # 編集
     path("delete/<int:pk>/", views.Delete.as_view(), name="delete"),
-    path("item_list/", views.Item_list.as_view(), name="item_list"),
     path("item_detail/<int:pk>/", views.Item_detail.as_view(), name="item_detail"), # 詳細
     path("add_item/<int:pk>/", views.add_item, name="add_item"),
     path("carts/", views.Carts.as_view(), name="carts"),
