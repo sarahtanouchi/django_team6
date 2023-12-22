9ef7b0aa9269ca31c61cb02cdfe2e19448911f08
[ユーザー名] <[sarah]>
2023-12-22 13:12:33 +0000

    フィルター機能作成

--- config/settings.py
+++ config/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 AUTH_USER_MODEL = "accounts.CustomUser"
 
 LOGIN_URL = "accounts:login"
-LOGIN_REDIRECT_URL = "items:index"
+LOGIN_REDIRECT_URL = "home"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
