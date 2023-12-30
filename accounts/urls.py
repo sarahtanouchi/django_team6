b4e7a245aecd75a6f19139e3e4481ff2b9f1f59e
EC2 Default User <ec2-user@ip-172-31-2-60.ec2.internal>
2023-12-30 15:18:20 +0000

    エラー

---
+++ accounts/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, reverse_lazy
+from . import views
+from django.contrib.auth import views as auth_views
+
+ 
+app_name = "accounts"
+ 
+urlpatterns = [
+    path("sign_up/", views.SignUp.as_view(), name="sign_up"),
+    path("login/", views.Login.as_view(), name="login"), # ログイン
+    path("logout/", views.Logout.as_view(), name="logout"), # ログアウト
+    path("detail/<int:pk>/", views.Detail.as_view(), name="detail"), # ユーザー詳細
+    path("update/<int:pk>/", views.Update.as_view(), name="update"), # 編集
+    # path("favorite/", views.Favorite.as_view(), name="favorite"), # お気に入り
+]
